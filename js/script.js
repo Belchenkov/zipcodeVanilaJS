058356ccbc055ea1b4366135422bddbf02de3b3e
Belchenkov <u608110@gmail.com>
2017-10-14 07:27:11 +0300

    Output Info for search location

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 // Listen for submit
 document.querySelector('#zipform').addEventListener('submit', getLocationInfo);
 
+// Listen for delete
+document.querySelector('body').addEventListener('click', deleteLocation);
+
 function clearMessage() {
     document.querySelector('#output').innerHTML = '';
+}
+
+function showIcon(icon) {
+    // Clear icons
+    document.querySelector('.icon-remove').style.display = 'none';
+    document.querySelector('.icon-check').style.display = 'none';
+
+    // Show correct icon
+    document.querySelector(`.icon-${icon}`).style.display = 'inline-flex';
+}
+
+// Delete location box
+function deleteLocation(e) {
+    e.preventDefault();
+
+    if (e.target.className == 'delete') {
+        console.log('delete');
+    }
 }
 
 function getLocationInfo(e) {
@@ -15,6 +36,7 @@ function getLocationInfo(e) {
     fetch(`http://api.zippopotam.us/us/${zip}`)
         .then(response => {
             if (response.status != 200) {
+                showIcon('remove');
                 document.querySelector('#output').innerHTML = 
                 `
                    <article class="message is-danger">
@@ -25,10 +47,36 @@ function getLocationInfo(e) {
                 setTimeout(clearMessage, 4000);
                 throw Error(response.statusText);
             } else {
+                showIcon('check');
                 return response.json();
             }
         })
-        .then(data => console.log(data))
+        .then(data => {
+            // Show location info
+            let output = '';
+            data.places.forEach(place => {
+                output += 
+                `
+                    <article class="message is-primary">
+                        <div class="message-header">
+                            <p>Location Info</p>
+                            <button class="delete"></button>
+                        </div> 
+                        <div class="message-body">
+                            <ul>
+                                <li><strong>City: </strong> ${place['place name']}</li>
+                                <li><strong>State: </strong> ${place['state']}</li>
+                                <li><strong>Longitude: </strong> ${place['longitude']}</li>
+                                <li><strong>Latitude: </strong> ${place['latitude']}</li>
+                            </ul>
+                        </div>
+                    </article> 
+                `
+            });
+
+            // Insert into output div
+            document.querySelector('#output').innerHTML = output;
+        })
         .catch(err => console.log(err));
 
 }
